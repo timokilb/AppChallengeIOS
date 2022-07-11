a9e22881ba20eae1c669c04a1647c7965eb309b1
Timo Kilb <timo@swinx.app>
2022-07-11 15:47:02 +0200

    Initial Commit

---
+++ AppChallengeIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppChallengeIOS
+//
+//  Created by Timo Kilb  on 11.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
